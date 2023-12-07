f5b949e6f34f095276c0d3deadbd624d80d975dc
EmmaRL <110081353+NinjaXDarking@users.noreply.github.com>
2023-12-06 23:34:43 -0600

    Update global.php

--- config/global.php
+++ config/global.php
@@ -1,15 +1,15 @@
 <?php 
 //Ip de la pc servidor de base de datos
-define("DB_HOST","localhost");
+define("DB_HOST","monorail.proxy.rlwy.net");
 
 //Nombre de la base de datos
-define("DB_NAME", "examenweb");
+define("DB_NAME", "railway");
 
 //Usuario de la base de datos
 define("DB_USERNAME", "root");
 
 //Contraseña del usuario de la base de datos
-define("DB_PASSWORD", "emanuel12");
+define("DB_PASSWORD", "1GAc1bdAed1h3C441Ee1cBFadh55-4A6");
 
 //definimos la codificación de los caracteres
 define("DB_ENCODE","utf8");
